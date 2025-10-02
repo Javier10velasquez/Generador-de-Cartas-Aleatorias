7adb20ea698cf7a48129ea7fce82ffb20c38d072
JV <149593608+Javier10velasquez@users.noreply.github.com>
2025-10-02 00:08:02 +0000

    cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numeros = ["A","J","Q","K","2","3","4","5","6","7","8","9"];
+
+  let grupoPalos = [
+    { simbolo: "♠", color: "black" }, 
+    { simbolo: "♣", color: "black" },  
+    { simbolo: "♥", color: "red" },   
+    { simbolo: "♦", color: "red" }     
+  ];
+
+  let indicePalo = Math.floor(Math.random() * grupoPalos.length);
+  let palo = grupoPalos[indicePalo];
+
+
+  let indiceNumero = Math.floor(Math.random() * numeros.length);
+  let numero = numeros[indiceNumero];
+
+
+  document.getElementById("arriba").innerHTML =
+    `<span style="color:${palo.color}">${palo.simbolo}</span>`;
+
+  document.getElementById("numero").innerHTML =
+    `<span style="color:${palo.color}">${numero}</span>`;
+
+  document.getElementById("abajo").innerHTML =
+    `<span style="color:${palo.color}">${palo.simbolo}</span>`;
 };
